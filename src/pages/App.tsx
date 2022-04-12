7e3c8720eec108e0bef6dcb7a5f0b24c63f87f4c
Bruno Durante <brunodurantec@gmail.com>
2022-04-12 15:33:05 -0300

    Criando Cronometro e Relogio

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Botao from '../components/Botao';
+import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './App.module.scss';
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario />
+      <Cronometro />
       <Lista />
     </div>
   );
