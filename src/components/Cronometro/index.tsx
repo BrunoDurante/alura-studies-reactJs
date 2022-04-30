c909295a5baf65a5136ca5ac7b5d4a109afce6b0
Bruno Durante <brunodurantec@gmail.com>
2022-04-30 19:29:43 -0300

    Criando função para converter o tempo adicionado para o cronometro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,8 +1,10 @@
+import { tempoParaSegundos } from '../../commom/utils/date';
 import Botao from '../Botao';
 import style from './Cronometro.module.scss';
 import Relogio from './Relogio';
 
 export default function Cronometro(){
+    console.log('Conversao: ', tempoParaSegundos('01:01:01'));
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>
