3a1b4ae006b0493d4392fa64ce56157c5e0739b6
Bruno Durante <brunodurantec@gmail.com>
2022-04-30 19:46:30 -0300

    Criando state para o tempo ser atualizado

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,14 +1,25 @@
-import { tempoParaSegundos } from '../../commom/utils/date';
+import { useState } from 'react';
+import { tempoParaSegundos } from '../../commom/utils/time';
+import { ITarefa } from '../../types/tarefa';
 import Botao from '../Botao';
 import style from './Cronometro.module.scss';
 import Relogio from './Relogio';
 
-export default function Cronometro(){
-    console.log('Conversao: ', tempoParaSegundos('01:01:01'));
+interface Props{
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({selecionado} : Props ){
+    const [tempo, setTempo] = useState<number>();
+    if(selecionado?.tempo){
+        setTempo(tempoParaSegundos(selecionado.tempo))
+    }
+
     return(
         <div className={style.cronometro}>
             <p className={style.titulo}>
                 Escolha um card e inicie o cronômetro!
+                Tempo: {tempo}
             </p>
             <div className={style.relogioWrapper}>
                 <Relogio />
